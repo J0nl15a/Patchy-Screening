cc0678cf684ed96810020c529fb90148f833d9ff
Jonah Conley <ARIJCONL@ljmu.ac.uk>
2025-05-24 20:13:39 +0100

    Streamlining sampling code to use sample name as input.

--- FLAMINGO_halo_sampling.py
+++ FLAMINGO_halo_sampling.py
@@ -5,6 +5,10 @@ import h5py
 from imp_patchy_screening import patchyScreening
 import sys
 
+sample_z = sys.argv[2]
+im = float(sys.argv[3])
+im_name = f"{float(sys.argv[3]):.1f}".replace('.', 'p')
+
 halo_z_bins = np.genfromtxt(
     '/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/FLAMINGO_halo_redshift_values.txt',
     dtype=[('i',   'i4'),
@@ -13,11 +17,7 @@ halo_z_bins = np.genfromtxt(
     delimiter=None
 )
 
-blue_sample = np.loadtxt('/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/dndz_galaxies_sampled_Blue_ntotal.txt')
-green_sample = np.loadtxt('/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/dndz_galaxies_sampled_Green_ntotal.txt')
-
-im = float(sys.argv[2])
-im_name = f"{float(sys.argv[2]):.1f}".replace('.', 'p')
+dndz_sample = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/dndz_galaxies_sampled_{sample_z}_ntotal.txt')
 
 def process_snapshot(i):
     """Process a single redshift‐bin index i.  
@@ -30,17 +30,11 @@ def process_snapshot(i):
                          0, 1, 0,
                          lightcone_method=('FULL','shell'))
     ps.filter_stellar_mass()
-    #print(f"File /cosma8/data/dp004/flamingo/Runs/L1000N1800/HYDRO_FIDUCIAL/hbt_lightcone_halos/lightcone0/lightcone_halos_{77-i:04d}.hdf5 doesn't work.")
 
-    # sample blue
-    n_blue = int(blue_sample[i][2])
-    subdf_blue = ps.merge.sample(n=n_blue, random_state=10)
-
-    # sample green
-    n_green = int(green_sample[i][2])
-    subdf_green = ps.merge.sample(n=n_green, random_state=1000)
-
-    return subdf_blue, subdf_green
+    nsamp = int(dndz_sample[i][2])
+    subdf = ps.merge.sample(n=nsamp, random_state=1000)
+    
+    return subdf
 
 # dispatch in parallel
 results = Parallel(n_jobs=int(sys.argv[1]),   # adjust to your cores
@@ -49,21 +43,15 @@ results = Parallel(n_jobs=int(sys.argv[1]),   # adjust to your cores
     for i in halo_z_bins['i']
 )
 
-# filter out the None’s and unzip
-blue_chunks  = [r[0] for r in results if r is not None]
-green_chunks = [r[1] for r in results if r is not None]
+print(results)
 
 # concatenate once at the end
-sampled_halo_data_blue  = pd.concat(blue_chunks,  ignore_index=True)
-sampled_halo_data_green = pd.concat(green_chunks, ignore_index=True)
+dfs = [df for df in results if df is not None]
+sampled_halo_data = pd.concat(dfs, ignore_index=True)
 
-mvir_blue=np.asarray(sampled_halo_data_blue.m_vir)
-nhalo_blue=len(mvir_blue)
-print(nhalo_blue)
-mvir_green=np.asarray(sampled_halo_data_green.m_vir)
-nhalo_green=len(mvir_green)
-print(nhalo_green)
+mvir=np.asarray(sampled_halo_data.m_vir)
+nhalo=len(mvir)
+print(nhalo)
 
 # write out
-sampled_halo_data_blue.to_parquet(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/sampled_halo_data_Blue_ntotal_{im_name}.parquet',  compression='snappy', index=False)
-sampled_halo_data_green.to_parquet(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/sampled_halo_data_Green_ntotal_{im_name}.parquet', compression='snappy', index=False)
+sampled_halo_data.to_parquet(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/sampled_halo_data_{sample_z}_ntotal_{im_name}.parquet',  compression='snappy', index=False)

2825580a4ae12dad9c240db8a6f612eba6586dea
Jonah Conley <ARIJCONL@ljmu.ac.uk>
2026-04-16 14:24:01 +0100

    Adapting emulator to predict galaxy abundance from stellar cut parameters. Included robust check against bad/negative spectra in training.

--- emulator_data_loader.py
+++ emulator_data_loader.py
@@ -1,55 +1,73 @@
 import numpy as np
 from pathlib import Path
+from scipy.signal import savgol_filter
 
-def data_loader(box, isim, iz, low_halo_threshold=True, negative_power_threshold=False, shot_noise_included=False, lightcone=0):
-    
-    min_amplitude = 10.3
-    max_amplitude = 11.3
-    amplitude_step = 0.1
-    min_slope = 0.0
-    max_slope = 1.0
-    slope_step = 0.1
+def data_loader(spectra, box, isim, iz, low_halo_threshold=True, negative_power_threshold=False, shot_noise_included=False, lightcone=0, abundance_cut=0.05,
+                 amp_min=10.3, amp_max=11.3, amp_step=0.1, slope_min=0.0, slope_max=1.0, slope_step=0.1):
 
-    cut_amplitude = np.repeat(np.arange(min_amplitude, max_amplitude + amplitude_step, amplitude_step).reshape(-1,1), int((max_slope - min_slope) / slope_step) + 1, axis=0)
-    cut_slope = np.tile(np.arange(min_slope, max_slope + slope_step, slope_step), int((max_amplitude - min_amplitude) / amplitude_step) + 1).reshape(-1,1)
+    cut_amplitude = np.repeat(np.arange(amp_min, amp_max + amp_step, amp_step).reshape(-1,1), int((slope_max - slope_min) / slope_step) + 1, axis=0)
+    cut_slope = np.tile(np.arange(slope_min, slope_max + slope_step, slope_step), int((amp_max - amp_min) / amp_step) + 1).reshape(-1,1)
     x_train = np.column_stack((np.round(cut_amplitude, 1), np.round(cut_slope, 1))) #Amplitude and slope parameters
 
-    initial_cross_spectrum = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/kappa_galaxy/{box}/{isim}/{iz}/lightcone{lightcone}/kappa_galaxy_power_spectrum_10p8_0p5.txt', 
-                                        delimiter=' ', skiprows=1, usecols=(0,1))
-    initial_auto_spectrum = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/galaxy_galaxy/{box}/{isim}/{iz}/lightcone{lightcone}/galaxy_galaxy_power_spectrum_10p8_0p5.txt', 
-                                        delimiter=' ', skiprows=1, usecols=(0,2) if not shot_noise_included else (0,1))
+    w = 5
+    p = 2
+    amp_min_name = f"{float(amp_min):.1f}".replace('.', 'p')
+    slope_min_name = f"{float(slope_min):.1f}".replace('.', 'p')
+    ell_bins = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/kappa_galaxy/{box}/{isim}/{iz}/lightcone{lightcone}/kappa_galaxy_power_spectrum_{amp_min_name}_{slope_min_name}.txt', 
+                            delimiter=' ', skiprows=1, usecols=(0))
+    ell_1000_mask = np.where(ell_bins > 1000)
+
+    # initial_cross_spectrum = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/kappa_galaxy/{box}/{isim}/{iz}/lightcone{lightcone}/kappa_galaxy_power_spectrum_10p8_0p5.txt', 
+    #                                     delimiter=' ', skiprows=1, usecols=(0,1))
+    # initial_auto_spectrum = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/galaxy_galaxy/{box}/{isim}/{iz}/lightcone{lightcone}/galaxy_galaxy_power_spectrum_10p8_0p5.txt', 
+    #                                     delimiter=' ', skiprows=1, usecols=(0,2) if not shot_noise_included else (0,1))
 
     negative_power = []
     for i, (a, s) in enumerate(x_train):
         amp_name = f"{float(a):.1f}".replace('.', 'p')
         slope_name = f"{float(s):.1f}".replace('.', 'p')
-        if i == 0:
-            cross_spectrum = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/kappa_galaxy/{box}/{isim}/{iz}/lightcone{lightcone}/kappa_galaxy_power_spectrum_{amp_name}_{slope_name}.txt', 
-                                        delimiter=' ', skiprows=1, usecols=(0,1))
-            auto_spectrum = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/galaxy_galaxy/{box}/{isim}/{iz}/lightcone{lightcone}/galaxy_galaxy_power_spectrum_{amp_name}_{slope_name}.txt', 
-                                        delimiter=' ', skiprows=1, usecols=(0,2) if not shot_noise_included else (0,1)) 
-            y_train_cross = cross_spectrum[:,1].reshape(1, -1) #/ initial_cross_spectrum[:,1]
-            y_train_auto = auto_spectrum[:,1].reshape(1, -1) #/ initial_auto_spectrum[:,1]
-            if negative_power_threshold:
-                negative_mask = np.where(cross_spectrum < 0)[0]
-                if len(negative_mask) > 0:
-                    negative_power.append(i)
+        print(a,s)
+        cross_spectrum = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/kappa_galaxy/{box}/{isim}/{iz}/lightcone{lightcone}/kappa_galaxy_power_spectrum_{amp_name}_{slope_name}.txt', 
+                                    delimiter=' ', skiprows=1, usecols=(1))
+        cross_spectrum_unsmoothed_component = cross_spectrum[np.where(ell_bins <= 1000)]
+        cross_spectrum_smooth_component = savgol_filter(cross_spectrum[ell_1000_mask], window_length=w, polyorder=p)
+        cross_spectrum = np.concatenate((cross_spectrum_unsmoothed_component, cross_spectrum_smooth_component))
+
+        auto_spectrum = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/galaxy_galaxy/{box}/{isim}/{iz}/lightcone{lightcone}/galaxy_galaxy_power_spectrum_{amp_name}_{slope_name}.txt', 
+                                    delimiter=' ', skiprows=1, usecols=(2) if not shot_noise_included else (1)) 
+        auto_spectrum_unsmoothed_component = auto_spectrum[np.where(ell_bins <= 1000)]
+        auto_spectrum_smooth_component = savgol_filter(auto_spectrum[ell_1000_mask], window_length=w, polyorder=p)
+        auto_spectrum = np.concatenate((auto_spectrum_unsmoothed_component, auto_spectrum_smooth_component))
+        
+        with open(f"./data_files/dndz_samples/{box}/{isim}/{iz}/lightcone{lightcone}/dndz_galaxies_sampled_{amp_name}_{slope_name}.txt", "r") as f:
+            first_line = f.readline().strip()
+            number = float(first_line.split(":")[-1])
+
+        if not np.all(np.isfinite(cross_spectrum)):
+            print(f"Non-finite cross spectrum at amp={a}, slope={s}")
+        if not np.all(np.isfinite(auto_spectrum)):
+            print(f"Non-finite auto spectrum at amp={a}, slope={s}")
+
+        if i==0:
+            y_train_cross = cross_spectrum.reshape(1, -1) #/ initial_cross_spectrum[:,1]
+            y_train_auto = auto_spectrum.reshape(1, -1) #/ initial_auto_spectrum[:,1]
+            y_train_abundance = number
             #std = initial_spectrum[:,2].reshape(1, -1)
         else:
-            print(a,s)
-            cross_spectrum = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/kappa_galaxy/{box}/{isim}/{iz}/lightcone{lightcone}/kappa_galaxy_power_spectrum_{amp_name}_{slope_name}.txt', 
-                                        delimiter=' ', skiprows=1, usecols=1) 
-            auto_spectrum = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/galaxy_galaxy/{box}/{isim}/{iz}/lightcone{lightcone}/galaxy_galaxy_power_spectrum_{amp_name}_{slope_name}.txt', 
-                                        delimiter=' ', skiprows=1, usecols=2 if not shot_noise_included else 1) 
-            y_train_cross = np.vstack((y_train_cross, cross_spectrum)) #/initial_cross_spectrum[:,1])) 
-            y_train_auto = np.vstack((y_train_auto, auto_spectrum)) #/initial_auto_spectrum[:,1]))
-            if negative_power_threshold:
-                negative_mask = np.where(cross_spectrum < 0)[0]
-                if len(negative_mask) > 0:
-                    negative_power.append(i)
+            y_train_cross = np.vstack((y_train_cross, cross_spectrum)) #/ initial_cross_spectrum[:,1]) 
+            y_train_auto = np.vstack((y_train_auto, auto_spectrum)) #/ initial_auto_spectrum[:,1])
+            y_train_abundance = np.vstack((y_train_abundance, number))
             #std = np.vstack((std, np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/{directory_name}/{isim}_{iz}_{amp_name}_{slope_name}.txt', 
             #                                delimiter=' ', skiprows=1, usecols=3 if spectra=='auto' else 2)))
-            print(y_train_cross.shape)
+
+
+        if negative_power_threshold:
+            negative_mask_auto = np.where(auto_spectrum < 0)[0]
+            negative_mask_cross = np.where(cross_spectrum < 0)[0]
+            if len(negative_mask_auto) > 0 or len(negative_mask_cross) > 0:
+                negative_power.append(i)
+
+        print(y_train_cross.shape)
 
     if negative_power_threshold:
         x_train = np.delete(x_train, negative_power, 0)
@@ -62,7 +80,7 @@ def data_loader(box, isim, iz, low_halo_threshold=True, negative_power_threshold
     elif iz == 'Green':
         kusiak_observed_nbar_per_sq_deg = 1846
     kusiak_observed_nbar_full_sky = kusiak_observed_nbar_per_sq_deg * 41253  # total sq deg in sky
-    nhalo_lower_bound = kusiak_observed_nbar_full_sky * 0.05  # 50% abundance cut
+    nhalo_lower_bound = kusiak_observed_nbar_full_sky * abundance_cut  # 95% abundance cut
 
     low_nhalos = []
     if low_halo_threshold:
@@ -71,13 +89,15 @@ def data_loader(box, isim, iz, low_halo_threshold=True, negative_power_threshold
             slope_name = f"{float(slope):.1f}".replace('.', 'p')
             with open(f"./data_files/dndz_samples/{box}/{isim}/{iz}/lightcone{lightcone}/dndz_galaxies_sampled_{im_name}_{slope_name}.txt", "r") as f:
                 first_line = f.readline().strip()
-                number = int(first_line.split(":")[-1])
+                number = float(first_line.split(":")[-1])
                 if number < nhalo_lower_bound:
                     low_nhalos.append(i)
         x_train = np.delete(x_train, low_nhalos, 0)
         y_train_cross = np.delete(y_train_cross, low_nhalos, 0)
         y_train_auto = np.delete(y_train_auto, low_nhalos, 0)
+
     print(y_train_cross.shape)
+    
 
     cut_values = np.column_stack((np.round(cut_amplitude, 1), np.round(cut_slope, 1)))
 
@@ -119,7 +139,7 @@ def data_loader(box, isim, iz, low_halo_threshold=True, negative_power_threshold
         y, c = diagonal_limit()
 
         try:
-            slope_limit = np.where(y == max_slope)[0][-1]
+            slope_limit = np.where(y == slope_max)[0][-1]
         except IndexError:
             slope_limit = 0
         plateau_point = low_nhalos_x[slope_limit]
@@ -131,8 +151,8 @@ def data_loader(box, isim, iz, low_halo_threshold=True, negative_power_threshold
 
     elif not low_halo_threshold:
         plateau_point = max(x_train[:,0])
-        m = 0.0
         c = 1.0
+        vertical_limit = max(x_train[:,0])
         
     # cut_values_negative_power = np.delete(cut_values.copy(), negative_power, 0)
     # unique_first_col_negative_power = np.unique(cut_values_negative_power[:,0])
@@ -148,21 +168,37 @@ def data_loader(box, isim, iz, low_halo_threshold=True, negative_power_threshold
 
     path = f'./gpy_model/{box}/{isim}/{iz}/lightcone{lightcone}/training/'
     prior_path = Path(path+'prior_limits.npy')
-    x_train_path = Path(path+'X_training_data.npy')
-    y_train_cross_path = Path(path+'Y_training_data_cross.npy')
-    y_train_auto_path = Path(path+'Y_training_data_auto.npy')
+    if spectra == 'auto' or spectra == 'cross':
+        x_train_path_auto = Path(path+'X_training_data_auto.npy')
+        x_train_path_cross = Path(path+'X_training_data_cross.npy')
+        y_train_cross_path = Path(path+'Y_training_data_cross.npy')
+        y_train_auto_path = Path(path+'Y_training_data_auto.npy')
+    elif spectra == 'abundance':
+        x_train_path_abundance = Path(path+'X_training_data_abundance.npy')
+        y_train_abundance_path = Path(path+'Y_training_data_abundance.npy')
+
     prior_path.parent.mkdir(parents=True, exist_ok=True)
-    x_train_path.parent.mkdir(parents=True, exist_ok=True)
-    y_train_cross_path.parent.mkdir(parents=True, exist_ok=True)
-    y_train_auto_path.parent.mkdir(parents=True, exist_ok=True)
+    if spectra == 'auto' or spectra == 'cross':
+        x_train_path_auto.parent.mkdir(parents=True, exist_ok=True)
+        x_train_path_cross.parent.mkdir(parents=True, exist_ok=True)
+        y_train_cross_path.parent.mkdir(parents=True, exist_ok=True)
+        y_train_auto_path.parent.mkdir(parents=True, exist_ok=True)
+    elif spectra == 'abundance':
+        x_train_path_abundance.parent.mkdir(parents=True, exist_ok=True)
+        y_train_abundance_path.parent.mkdir(parents=True, exist_ok=True)
 
     # np.save(f'./gpy_model/training/prior_limits_{isim}_{iz}.npy', np.array([plateau_point, m, c]))
     print(plateau_point, c, vertical_limit)
     # quit()
     np.save(prior_path, np.array([plateau_point, c, vertical_limit]))
-    np.save(x_train_path, x_train)
-    np.save(y_train_cross_path, y_train_cross)
-    np.save(y_train_auto_path, y_train_auto)
+    if spectra == 'auto' or spectra == 'cross':
+        np.save(x_train_path_auto, x_train)
+        np.save(x_train_path_cross, x_train)
+        np.save(y_train_cross_path, y_train_cross)
+        np.save(y_train_auto_path, y_train_auto)
+    elif spectra == 'abundance':
+        np.save(x_train_path_abundance, x_train)
+        np.save(y_train_abundance_path, y_train_abundance)
 
     return
 
--- mock_catalog_emulator.py
+++ mock_catalog_emulator.py
@@ -16,27 +16,30 @@ from pathlib import Path
 # spec.loader.exec_module(module)
 
 def emulator(x, spectra, box, isim, iz, 
-             save=False, load=False, log=True, oos_test=False, retrain=False, lightcone=0):
+             save=False, load=False, log=True, oos_test=False, retrain=False, lightcone=0, abundance_cut=0.05,
+             amp_min=10.3, amp_max=11.3, amp_step=0.1, slope_min=0.0, slope_max=1.0, slope_step=0.1):
     
     try:
-        x_train = np.load(f"./gpy_model/{box}/{isim}/{iz}/lightcone{lightcone}/training/X_training_data.npy")
+        x_train = np.load(f"./gpy_model/{box}/{isim}/{iz}/lightcone{lightcone}/training/X_training_data_{spectra}.npy")
         y_train = np.load(f"./gpy_model/{box}/{isim}/{iz}/lightcone{lightcone}/training/Y_training_data_{spectra}.npy")
     except FileNotFoundError:
-        data_loader(box, isim, iz, lightcone=lightcone)
-        x_train = np.load(f"./gpy_model/{box}/{isim}/{iz}/lightcone{lightcone}/training/X_training_data.npy")
+        data_loader(spectra, box, isim, iz, lightcone=lightcone, abundance_cut=abundance_cut,
+                    amp_min=amp_min, amp_max=amp_max, amp_step=amp_step, slope_min=slope_min, slope_max=slope_max, slope_step=slope_step)
+        x_train = np.load(f"./gpy_model/{box}/{isim}/{iz}/lightcone{lightcone}/training/X_training_data_{spectra}.npy")
         y_train = np.load(f"./gpy_model/{box}/{isim}/{iz}/lightcone{lightcone}/training/Y_training_data_{spectra}.npy")
     if retrain:
-        data_loader(box, isim, iz, low_halo_threshold=True, negative_power_threshold=False, shot_noise_included=False, lightcone=lightcone)
-        x_train = np.load(f"./gpy_model/{box}/{isim}/{iz}/lightcone{lightcone}/training/X_training_data.npy")
+        data_loader(spectra, box, isim, iz, low_halo_threshold=True, negative_power_threshold=False, shot_noise_included=False, lightcone=lightcone, abundance_cut=abundance_cut,
+                    amp_min=amp_min, amp_max=amp_max, amp_step=amp_step, slope_min=slope_min, slope_max=slope_max, slope_step=slope_step)
+        x_train = np.load(f"./gpy_model/{box}/{isim}/{iz}/lightcone{lightcone}/training/X_training_data_{spectra}.npy")
         y_train = np.load(f"./gpy_model/{box}/{isim}/{iz}/lightcone{lightcone}/training/Y_training_data_{spectra}.npy")
         print(y_train)
-        if spectra == 'auto':
-            dir_type = 'galaxy_galaxy'
-        elif spectra == 'cross':
-            dir_type = 'kappa_galaxy'
-        initial_spectra = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/{dir_type}/{box}/{isim}/{iz}/lightcone{lightcone}/{dir_type}_power_spectrum_10p8_0p5.txt', 
-                                    delimiter=' ', skiprows=1, usecols=0)
-                                    # delimiter=' ', skiprows=1, usecols=2 if spectra=='auto' else 1)
+        # if spectra == 'auto' or spectra == 'abundance':
+        #     dir_type = 'galaxy_galaxy'
+        # elif spectra == 'cross':
+        #     dir_type = 'kappa_galaxy'
+        # initial_spectra = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/{dir_type}/{box}/{isim}/{iz}/lightcone{lightcone}/{dir_type}_power_spectrum_10p8_0p5.txt', 
+        #                             delimiter=' ', skiprows=1, usecols=0)
+        #                             # delimiter=' ', skiprows=1, usecols=2 if spectra=='auto' else 1)
 
     x_train_min = (np.min(x_train[:,0]), np.min(x_train[:,1]))
     x_train_max = (np.max(x_train[:,0]), np.max(x_train[:,1]))
@@ -49,7 +52,14 @@ def emulator(x, spectra, box, isim, iz,
             if a == x[0] and s == x[1]:
                 print('DELETING!')
                 x_train_normalised = np.delete(x_train_normalised, (i), axis=0)
-                y_train =  np.delete(y_train, (i), axis=0)
+                y_train = np.delete(y_train, (i), axis=0)
+
+    if not np.all(np.isfinite(y_train)):
+        raise ValueError("y_train contains non-finite values before log10")
+
+    if log and np.any(y_train <= 0):
+        bad = np.argwhere(y_train <= 0)
+        raise ValueError(f"y_train has non-positive entries; first few: {bad}")
 
     if log:
         y_train_normalised = np.log10(y_train.copy())
@@ -128,9 +138,16 @@ if __name__ == '__main__':
     lightcone = int(sys.argv[5])
 
     spectra = str(sys.argv[4])
-    amp = 10.7
-    slope = 0.636
-    residual = 0.083
+    amp = 10.808
+    slope = 0.257
+    residual = 0.0
+
+    if iz == 'Blue':
+        kusiak_nbar = 3409
+    elif iz == 'Green':
+        kusiak_nbar = 1846
+
+    kusiak_observed_abundance = kusiak_nbar * 41253
 
     test = np.array((amp+residual, slope))
     test_name = [f"{float(test[0]):.1f}".replace('.', 'p'), f"{float(test[1]):.1f}".replace('.', 'p')]
@@ -140,14 +157,19 @@ if __name__ == '__main__':
     else:
         test_name_plus_1 = [f"{float(amp+0.1):.1f}".replace('.', 'p'), f"{float(slope):.1f}".replace('.', 'p')]
 
-    if spectra == 'auto':
+    if spectra == 'auto' or spectra == 'abundance':
         dir_type = 'galaxy_galaxy'
     elif spectra == 'cross':
         dir_type = 'kappa_galaxy'
 
-    # true = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/{dir_type}/{box}/{isim}/{iz}/lightcone{lightcone}/{dir_type}_power_spectrum_{test_name[0]}_{test_name[1]}.txt', 
-    #                 # delimiter=' ', skiprows=1, usecols=(0,1) if spectra=='auto' else (0,1))
-    #                 delimiter=' ', skiprows=1, usecols=(0,2) if spectra=='auto' else (0,1))
+    true = np.loadtxt(f'/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/power_spectra/{dir_type}/{box}/{isim}/{iz}/lightcone{lightcone}/{dir_type}_power_spectrum_{test_name[0]}_{test_name[1]}.txt', 
+                    # delimiter=' ', skiprows=1, usecols=(0,1) if spectra=='auto' else (0,1))
+                    delimiter=' ', skiprows=1, usecols=(0,2) if spectra=='auto' else (0,1))
+    
+    with open(f"/cosma8/data/dp004/dc-conl1/FLAMINGO/patchy_screening/data_files/dndz_samples/{box}/{isim}/{iz}/lightcone{lightcone}/dndz_galaxies_sampled_{test_name[0]}_{test_name[1]}.txt", "r") as f:
+        first_line = f.readline().strip()
+        abundance = int(first_line.split(":")[-1])
+        print(f"Abundance for test point {test_name}: {abundance}")
     
     if amp+residual >= 11.3:
         pass
@@ -157,17 +179,109 @@ if __name__ == '__main__':
         #                         delimiter=' ', skiprows=1, usecols=(0,2) if spectra=='auto' else (0,1))
         pass
 
-    pred = emulator(test, spectra, box, isim, iz, save=True, retrain=True, oos_test=False, lightcone=lightcone)#, log=False)
-    print(pred)
-    quit()
-    #print(true[:, 1])
-    #print(pred - true[:, 1])
+    pred_train = emulator(test, spectra, box, isim, iz, save=True, retrain=True, lightcone=lightcone, 
+                    abundance_cut=0.05 if spectra != 'abundance' else 0.0, slope_max=2.0 if iz == 'Blue' else 1.0)
+    pred = emulator(test, spectra, box, isim, iz, load=True, lightcone=lightcone, 
+                    abundance_cut=0.05 if spectra != 'abundance' else 0.0, log=True, slope_max=2.0 if iz == 'Blue' else 1.0)
+    print(pred_train, abundance)
+    # print(pred)
+
+    if spectra == 'abundance':
+
+        # Confusion matrix for abundance > 50% of observed
+        threshold = 0.5 * kusiak_observed_abundance
+        tp = 0  # True Positive: both > threshold
+        tn = 0  # True Negative: both <= threshold
+        fp = 0  # False Positive: pred > threshold, true <= threshold
+        fn = 0  # False Negative: pred <= threshold, true > threshold
+
+        fp_list = []
+        fn_list = []
+        
+        x_train = np.load(f"./gpy_model/{box}/{isim}/{iz}/lightcone{lightcone}/training/X_training_data.npy")
+        pred_errors = []
+
+        for i in range(x_train.shape[0]): 
+
+            name_i = [f"{float(x_train[i,0]):.1f}".replace('.', 'p'), f"{float(x_train[i,1]):.1f}".replace('.', 'p')]
+            print(x_train[i,:])
+            pred_i = emulator(x_train[i,:], spectra, box, isim, iz, oos_test=True, load=True, lightcone=lightcone, 
+                              abundance_cut=0.0, log=True, slope_max=2.0 if iz == 'Blue' else 1.0)
+            print(pred_i)
+
+            with open(f"./data_files/dndz_samples/{box}/{isim}/{iz}/lightcone{lightcone}/dndz_galaxies_sampled_{name_i[0]}_{name_i[1]}.txt", "r") as f:
+                first_line = f.readline().strip()
+                true_i = float(first_line.split(":")[-1])
+
+            true_positive = true_i > threshold
+            pred_positive = pred_i > threshold
+            
+            if true_positive and pred_positive:
+                tp += 1
+            elif not true_positive and not pred_positive:
+                tn += 1
+            elif not true_positive and pred_positive:
+                fp += 1
+                fp_list.append((name_i, pred_i[0], true_i))
+            elif true_positive and not pred_positive:
+                fn += 1
+                fn_list.append((name_i, pred_i[0], true_i))
+
+            error_i = (pred_i)/true_i
+            error_i = np.array(error_i)
+            print(error_i)
+            pred_errors.append(error_i)
+
+        print(pred_errors)
+        pred_errors = np.array(pred_errors)
+        mean_pred_errors = np.mean(pred_errors, axis=0)
+        print(f"Mean Prediction Errors: {mean_pred_errors}")
+        std_pred_errors = np.std(pred_errors, axis=0)
+        print(f"Standard Deviation of Prediction Errors: {std_pred_errors}")
+
+        print(f"Confusion Matrix for abundance > {threshold}:")
+        print(f"True Positives (TP): {tp}")
+        print(f"True Negatives (TN): {tn}")
+        print(f"False Positives (FP): {fp}")
+        print(f"False Negatives (FN): {fn}")
+        
+        accuracy = (tp + tn) / (tp + tn + fp + fn) if (tp + tn + fp + fn) > 0 else 0
+        precision = tp / (tp + fp) if (tp + fp) > 0 else 0
+        recall = tp / (tp + fn) if (tp + fn) > 0 else 0
+        f1 = 2 * precision * recall / (precision + recall) if (precision + recall) > 0 else 0
+        
+        print(f"Accuracy: {accuracy:.3f}")
+        print(f"Precision: {precision:.3f}")
+        print(f"Recall: {recall:.3f}")
+        print(f"F1 Score: {f1:.3f}")
+
+        print("\nFalse Positives:")
+        for fp_entry in fp_list:
+            print(f"Parameters: {fp_entry[0]}, Predicted Abundance: {fp_entry[1]:.1f}, True Abundance: {fp_entry[2]:.1f}")
+        print("\nFalse Negatives:")
+        for fn_entry in fn_list:
+            print(f"Parameters: {fn_entry[0]}, Predicted Abundance: {fn_entry[1]:.1f}, True Abundance: {fn_entry[2]:.1f}")  
+        
+        # Plot confusion matrix
+        cm = np.array([[tp, fp], [fn, tn]])
+        pb.imshow(cm, cmap='Blues', interpolation='nearest')
+        pb.colorbar()
+        pb.xticks([0, 1], ['Predicted > threshold', 'Predicted <= threshold'])
+        pb.yticks([0, 1], ['Actual > threshold', 'Actual <= threshold'])
+        for i in range(2):
+            for j in range(2):
+                pb.text(j, i, cm[i, j], ha='center', va='center', color='black', fontsize=12)
+        pb.title(f'Confusion Matrix (threshold = {threshold:.0f})')
+        pb.savefig('./Plots/confusion_matrix_abundance.png', dpi=400)
+        pb.clf()
+
+    # quit()
 
     farren_data = np.loadtxt(f'./unWISExLens_lklh/data/v1.0/bandpowers/unWISExACT-DR6_{str(iz).lower()}_baseline_Clgg+Clkk+Clkg.dat', usecols=(0,1,3)).reshape(-1,3)
     ell_200_mask = np.where(farren_data[:,0] > 200)
 
     pb.loglog(farren_data[:,0][ell_200_mask], farren_data[:,1][ell_200_mask]*1e5 if spectra=='auto' else farren_data[:,2][ell_200_mask]*1e5, color='k', marker='.', markersize=5, label='ACT x unWISE (Farren et al. 2023)')
-    # pb.loglog(true[:,0], true[:,1], label=f'Mock catalog {amp+residual, slope}', color='b')
+    pb.loglog(true[:,0], true[:,1], label=f'Mock catalog {amp+residual, slope}', color='b')
     if amp+residual >= 11.3:
         pass
     else:
@@ -182,21 +296,21 @@ if __name__ == '__main__':
     pb.savefig('./Plots/mock_catalog_emulator_test.png', dpi=400)
     pb.clf()
 
-    # pb.hlines(y=1.000, xmin=-1, xmax=np.max(true[:,0])*1.1, color='k', linestyles='solid', alpha=0.5, label=None)
-    # pb.hlines(y=0.990, xmin=-1, xmax=np.max(true[:,0])*1.1, color='k', linestyles='dashed', alpha=0.5, label='1% error')
-    # pb.hlines(y=1.010, xmin=-1, xmax=np.max(true[:,0])*1.1, color='k', linestyles='dashed', alpha=0.5, label=None)
-    # pb.hlines(y=0.950, xmin=-1, xmax=np.max(true[:,0])*1.1, color='k', linestyles='dotted', alpha=0.5, label='5% error')
-    # pb.hlines(y=1.050, xmin=-1, xmax=np.max(true[:,0])*1.1, color='k', linestyles='dotted', alpha=0.5, label=None)
-    # pb.plot(true[:,0], pred/true[:,1], label='Error w.r.t. simulated clustering')
-    # #pb.plot(true[:,0], pred/(farren_data[:,1][ell_200_mask]*1e5) if spectra=='auto' else pred/(farren_data[:,2][ell_200_mask]*1e5), label='Error w.r.t. observed clustering')
-    # pb.title(f'Emulator error test (x_test: Amplitude={test[0]}, Slope={test[1]})')
-    # pb.xlabel('$\ell$')
-    # pb.ylabel('Residual')
-    # pb.xlim(100, 3000)
-    # pb.legend()
-    # pb.tight_layout()
-    # pb.savefig('./Plots/mock_catalog_emulator_error_test.png', dpi=400)
-    # pb.clf()
+    pb.hlines(y=1.000, xmin=-1, xmax=np.max(true[:,0])*1.1, color='k', linestyles='solid', alpha=0.5, label=None)
+    pb.hlines(y=0.990, xmin=-1, xmax=np.max(true[:,0])*1.1, color='k', linestyles='dashed', alpha=0.5, label='1% error')
+    pb.hlines(y=1.010, xmin=-1, xmax=np.max(true[:,0])*1.1, color='k', linestyles='dashed', alpha=0.5, label=None)
+    pb.hlines(y=0.950, xmin=-1, xmax=np.max(true[:,0])*1.1, color='k', linestyles='dotted', alpha=0.5, label='5% error')
+    pb.hlines(y=1.050, xmin=-1, xmax=np.max(true[:,0])*1.1, color='k', linestyles='dotted', alpha=0.5, label=None)
+    pb.plot(true[:,0], pred/true[:,1], label='Error w.r.t. simulated clustering')
+    pb.plot(true[:,0], pred/(farren_data[:,1][ell_200_mask]*1e5) if spectra=='auto' else pred/(farren_data[:,2][ell_200_mask]*1e5), label='Error w.r.t. observed clustering')
+    pb.title(f'Emulator error test (x_test: Amplitude={test[0]}, Slope={test[1]})')
+    pb.xlabel('$\ell$')
+    pb.ylabel('Residual')
+    pb.xlim(100, 3000)
+    pb.legend()
+    pb.tight_layout()
+    pb.savefig('./Plots/mock_catalog_emulator_error_test.png', dpi=400)
+    pb.clf()
     quit()
 
     x_train = np.load(f"./gpy_model/{box}/{isim}/{iz}/lightcone{lightcone}/training/X_training_data.npy")

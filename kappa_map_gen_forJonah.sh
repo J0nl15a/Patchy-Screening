b759137b4b420132c278cab54585a0f4158b2ee6
Jonah Conley <ARIJCONL@ljmu.ac.uk>
2026-07-15 14:30:33 +0100

    Updating kappa map submission script.

--- kappa_map_gen_forJonah.sh
+++ kappa_map_gen_forJonah.sh
@@ -2,18 +2,20 @@
 #SBATCH -o batch_files/kappa_map_gen_logs/job.%j_%a.out
 #SBATCH -e batch_files/kappa_map_gen_logs/job.%j_%a.err
 #SBATCH --job-name=kappa_map_gen_%j_%a    # Job name
-#SBATCH --array=0,3 ## for FIDUCIAL and LS8 run, please modify this accordingly
 #SBATCH --mem=200G ##300G for particle lc, rest 200G
 #SBATCH -p cosma8
 #SBATCH -A dp004
-#SBATCH --time=10:00:00
+#SBATCH --time=3:00:00
 pwd; hostname; date
+module purge
+conda activate patchy_screening
 
-START_NUM=$(( ($SLURM_ARRAY_TASK_ID - 1) * 1 + 1 ))
-END_NUM=$(( $SLURM_ARRAY_TASK_ID * 1 ))
-echo This is task $SLURM_ARRAY_TASK_ID, which will do runs $START_NUM to $END_NUM
+# START_NUM=$(( ($SLURM_ARRAY_TASK_ID - 1) * 1 + 1 ))
+# END_NUM=$(( $SLURM_ARRAY_TASK_ID * 1 ))
+# echo This is task $SLURM_ARRAY_TASK_ID, which will do runs $START_NUM to $END_NUM
 
 ## python3 kappa_map_gen_forJonah.py $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_ID > kappa_map_gen_forJonah_$SLURM_ARRAY_TASK_ID.log
-python3 kappa_map_gen_forJonah.py $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_ID
+# python3 kappa_map_gen_forJonah.py $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_ID
+python3 kappa_map_gen_forJonah.py "$@"
 
 date

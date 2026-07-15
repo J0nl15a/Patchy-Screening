be388535499333da7b94fe97e89045748b4ce9d3
Jonah Conley <ARIJCONL@ljmu.ac.uk>
2026-07-15 14:59:53 +0100

    Updating dpi of plots.

--- imp_tau_plot.py
+++ imp_tau_plot.py
@@ -47,7 +47,7 @@ class TauPlotter:
 
     def plot_by_stellar_bin(self, sim, mass_bin, slope_bin, colours, nside=8192, primary_method='FITS', file_method='unlensed', signal_flag=True, noise_data=None):
         # For a fixed stellar bin, plot tau profiles for multiple colours.
-        fig, ax = plt.subplots(figsize=(8,6), constrained_layout=True, dpi=1200)
+        fig, ax = plt.subplots(figsize=(8,6), constrained_layout=True, dpi=400)
         ax2 = ax.twiny()
         ax.hlines(y=0, xmin=-1, xmax=12, linestyles='-', color='k', label=None)
         line_colours = {'Blue':'tab:blue', 'Green':'tab:green', 'Red':'tab:red'}
@@ -77,12 +77,12 @@ class TauPlotter:
         file_suffix = "" if file_method==False else f"_{file_method}"
         signal_suffix = "" if signal_flag==True else "_no_ps"
         noise_suffix = "" if noise_data==None else "_noise"
-        plt.savefig(f"./Plots/{sim}_stellar_bin_{self.stellar_bins}_{self.slope}_nside{nside}{primary_suffix}{file_suffix}{signal_suffix}{noise_suffix}.png", dpi=1200)
+        plt.savefig(f"./Plots/{sim}_stellar_bin_{self.stellar_bins}_{self.slope}_nside{nside}{primary_suffix}{file_suffix}{signal_suffix}{noise_suffix}.png", dpi=400)
         plt.clf()
 
     def plot_by_colour(self, sim, colour, mass_bins, slope_bin, nside=8192, primary_method='FITS', file_method='unlensed', signal_flag=True, noise_data=None):
         # For a fixed colour, plot tau profiles for multiple stellar mass bins.
-        fig, ax = plt.subplots(figsize=(8,6), constrained_layout=True, dpi=1200)
+        fig, ax = plt.subplots(figsize=(8,6), constrained_layout=True, dpi=400)
         ax2 = ax.twiny()
         ax.hlines(y=0, xmin=-1, xmax=12, linestyles='-', color='k', label=None)
         alpha = [a for a in np.linspace(1.0, 0.2, len(mass_bins))] 
@@ -116,12 +116,12 @@ class TauPlotter:
         file_suffix = "" if file_method==False else f"_{file_method}"
         signal_suffix = "" if signal_flag==True else "_no_ps"
         noise_suffix = "" if noise_data==None else "_noise"
-        plt.savefig(f"./Plots/{sim}_sample_{colour}_{self.slope}_nside{nside}{primary_suffix}{file_suffix}{signal_suffix}{noise_suffix}.png", dpi=1200)
+        plt.savefig(f"./Plots/{sim}_sample_{colour}_{self.slope}_nside{nside}{primary_suffix}{file_suffix}{signal_suffix}{noise_suffix}.png", dpi=400)
         plt.clf()
 
     def plot_by_file_method(self, sim, colour, mass_bin, slope_bin, nside=8192, primary_method='FITS', file_methods='unlensed', signal_flag=True, noise_data=None):
         # For a fixed stellar bin and colour, plot tau profiles for different file method suffixes.
-        fig, ax = plt.subplots(figsize=(8,6), constrained_layout=True, dpi=1200)
+        fig, ax = plt.subplots(figsize=(8,6), constrained_layout=True, dpi=400)
         ax2 = ax.twiny()
         ax.hlines(y=0, xmin=-1, xmax=12, linestyles='-', color='k', label=None)
         line_colours = {'Blue':'tab:blue', 'Green':'tab:green', 'Red':'tab:red'}
@@ -164,7 +164,7 @@ class TauPlotter:
         primary_suffix = "_CAMB" if primary_method=='CAMB' else "_FITS"
         signal_suffix = "" if signal_flag==True else "_no_ps"
         noise_suffix = "" if noise_data==None else "_noise"
-        plt.savefig(f"./Plots/{sim}_method_comp_{colour}_{self.stellar_bins}_{self.slope}_nside{nside}{primary_suffix}{signal_suffix}{noise_suffix}.png", dpi=1200)
+        plt.savefig(f"./Plots/{sim}_method_comp_{colour}_{self.stellar_bins}_{self.slope}_nside{nside}{primary_suffix}{signal_suffix}{noise_suffix}.png", dpi=400)
         plt.clf()
 
     def generic_plot(self, file_list, labels, line_styles, colours, alpha, plot_title, outname, noise_data=None):
@@ -203,22 +203,40 @@ if __name__ == '__main__':
 
     tp = TauPlotter(base_dir="./L1000N1800")
     
-    '''tp.generic_plot(file_list=['./L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p77_0p878_nside8192_FITS_unlensed_ell_limited.pickle'],
-                    labels=["Optimized catalog method"],
-                    line_styles=["-"],
-                    colours=["tab:blue"],
-                    alpha=[1],
-                    plot_title=f'$\\tau$ Profiles for Blue sample, $\log M_*$=10.77, $n_{{cut}}$=0.878, with optimized mock catalog, \n(sim=HYDRO_FIDUCIAL, nside=8192)',      
-                    outname='HYDRO_FIDUCIAL_optimum_catalog_Blue_10p77_0p878_nside8192.png')
-    quit()'''
+    # tp.generic_plot(file_list=['./data_files/tau_profiles/L1000N1800//HYDRO_LOW_SIGMA8_tau_Mstar_bin10p725_0p894_nside8192_FITS_unlensed.pickle'],
+    #                 labels=["Optimized catalog method"],
+    #                 line_styles=["-"],
+    #                 colours=["tab:blue"],
+    #                 alpha=[1],
+    #                 plot_title=f'$\\tau$ Profiles for Blue sample, $\log M_*$=10.725, $n_{{cut}}$=0.894, with optimized mock catalog, \n(sim=HYDRO_FIDUCIAL, nside=8192)',      
+    #                 outname='HYDRO_LOW_SIGMA8_optimum_catalog_Blue_10p725_0p894_nside8192_unlensed.png')
+    # quit()
+
+    tp.generic_plot(file_list=['./data_files/tau_profiles/L1000N1800/HYDRO_FIDUCIAL/Green/lightcone0/tau_mle_catalogue_nside8192_FITS_unlensed.pickle',
+                               './data_files/tau_profiles/L1000N1800/HYDRO_FIDUCIAL/Green/lightcone0/tau_from_image_mle_catalogue_nside8192_FITS_unlensed.pickle'],
+                            #    './data_files/tau_profiles/L1000N1800/HYDRO_FIDUCIAL/Blue/lightcone0/tau_mle_catalogue_nside8192_FITS_unlensed_no_ps.pickle',
+                            #    './data_files/tau_profiles/L1000N1800/HYDRO_FIDUCIAL/Blue/lightcone0/tau_mle_catalogue_nside8192_FITS_lensed_z2.pickle',
+                            #    './data_files/tau_profiles/L1000N1800/HYDRO_FIDUCIAL/Blue/lightcone0/tau_mle_catalogue_nside8192_FITS_lensed_z2_no_ps.pickle',
+                            #    './data_files/tau_profiles/L1000N1800/HYDRO_FIDUCIAL/Blue/lightcone0/tau_mle_catalogue_nside8192_FITS_lensed_z3.pickle',
+                            #    './data_files/tau_profiles/L1000N1800/HYDRO_FIDUCIAL/Blue/lightcone0/tau_mle_catalogue_nside8192_FITS_lensed_z3_no_ps.pickle'],
+                            #    './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p808_0p259_nside8192_CAMB.pickle',
+                            #    './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p808_0p259_nside8192_CAMB_no_ps.pickle'],
+                    labels=["unlensed (from profile stack)", "unlensed (from image stack)"], #"unlensed (no PS)", "lensed to z=2", "lensed to z=2 (no PS)", "lensed to z=3", "lensed to z=3 (no PS)"],# "CAMB", "CAMB (no PS)"],
+                    line_styles=["-", ":"],# "--", "*", "-.", "+"],# "-", ":"],
+                    colours=["tab:green" for _ in range(2)], #+ ["darkblue" for _ in range(6)],
+                    alpha=[1. for _ in range(2)],
+                    plot_title='$\\tau$ Profiles for Green sample, MLE mock catalogue, for different lensing methods\n(sim=HYDRO_FIDUCIAL, nside=8192)',
+                    outname='HYDRO_FIDUCIAL_method_comp_full_Green_mle_catalogue_nside8192_FITS_image_stack.png')
+    quit()
     
-    tp.generic_plot(file_list=['./L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p77_0p878_nside8192_FITS_unlensed_ell_limited.pickle', 
-                               './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p77_0p878_nside8192_FITS_unlensed_no_ps_ell_limited.pickle', 
-                               './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p77_0p878_nside8192_FITS_lensed_z2_ell_limited.pickle', 
-                               './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p77_0p878_nside8192_FITS_lensed_z2_no_ps_ell_limited.pickle', 
-                               './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p77_0p878_nside8192_FITS_lensed_z3_ell_limited.pickle', 
-                               './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p77_0p878_nside8192_FITS_lensed_z3_no_ps_ell_limited.pickle',
-                               './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p77_0p878_nside8192_CAMB_ell_limited.pickle'],
+    tp.generic_plot(file_list=['./data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p808_0p259_nside8192_FITS_unlensed_ell_limited.pickle', 
+                               './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p808_0p259_nside8192_FITS_unlensed_no_ps_ell_limited.pickle', 
+                               './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p808_0p259_nside8192_FITS_lensed_z2_ell_limited.pickle', 
+                               './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p808_0p259_nside8192_FITS_lensed_z2_no_ps_ell_limited.pickle', 
+                               './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p808_0p259_nside8192_FITS_lensed_z3_ell_limited.pickle', 
+                               './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p808_0p259_nside8192_FITS_lensed_z3_no_ps_ell_limited.pickle',
+                               './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p808_0p259_nside8192_CAMB_ell_limited.pickle',
+                               './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p808_0p259_nside8192_CAMB_no_ps_ell_limited.pickle',],
                     #labels=["CAMB", "FITS", f"CAMB ($\ell$ limited, no signal)", "CAMB (scalar)", "FITS no signal"],
                     #line_styles=["--", "-", "-.", ":", ":"],
                     #colours=["tab:blue", "tab:red", "tab:green", "tab:orange", "tab:brown"],#for _ in range(4)],
@@ -226,14 +244,14 @@ if __name__ == '__main__':
                     #plot_title=f'$\\tau$ Profiles for Blue sample, $\log M_*$=10.7 for different\slopes of the z-dependant stellar cut\n(sim=HYDRO_FIDUCIAL, nside=8192)',                        
                     #plot_title=f'$\\tau$ Profiles for Blue sample, $\log M_*$=10.7, $n_{{cut}}$=0.0, for a difference in primary CMB method, \n(sim=HYDRO_FIDUCIAL, nside=8192)',      
                     #outname='HYDRO_FIDUCIAL_cmb_comp_Blue_10p7_0p0_nside8192.png')
-                    labels=["unlensed", "unlensed (no PS)", "lensed to z=2", "lensed to z=2 (no PS)", "lensed to z=3", "lensed to z=3 (no PS)", "CAMB"],
-                    line_styles=["-", ":", "--", "*", "-.", "+", "-"],
-                    colours=["tab:blue" for _ in range(6)] + ["darkblue"],
-                    alpha=[1. for _ in range(7)],
-                    plot_title=f'$\\tau$ Profiles for Blue sample, $\log M_*$=10.77, $n_{{cut}}$=0.878, for different lensing methods\n(sim=HYDRO_FIDUCIAL, nside=8192)',
-                    outname='HYDRO_FIDUCIAL_method_comp_full_Blue_10p77_0p878_nside8192_FITS.png')
+                    labels=["unlensed", "unlensed (no PS)", "lensed to z=2", "lensed to z=2 (no PS)", "lensed to z=3", "lensed to z=3 (no PS)", "CAMB", "CAMB (no PS)"],
+                    line_styles=["-", ":", "--", "*", "-.", "+", "-", ":"],
+                    colours=["tab:blue" for _ in range(6)] + ["darkblue" for _ in range(2)],
+                    alpha=[1. for _ in range(8)],
+                    plot_title=f'$\\tau$ Profiles for Blue sample, $\log M_*$=10.808, $n_{{cut}}$=0.259, for different lensing methods\n(sim=HYDRO_FIDUCIAL, nside=8192)',
+                    outname='HYDRO_FIDUCIAL_method_comp_full_Blue_10p808_0p259_nside8192_FITS_ell_limited.png')
     quit()
-    tp.generic_plot(file_list=['./L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p0_nside8192_CAMB.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p1_nside8192_CAMB.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p2_nside8192_CAMB.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p3_nside8192_CAMB.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p4_nside8192_CAMB.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p5_nside8192_CAMB.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p6_nside8192_CAMB.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p7_nside8192_CAMB.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p8_nside8192_CAMB.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p9_nside8192_CAMB.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin10p7_1p0_nside8192_CAMB.pickle'],
+    tp.generic_plot(file_list=['./data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p0_nside8192_CAMB.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p1_nside8192_CAMB.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p2_nside8192_CAMB.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p3_nside8192_CAMB.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p4_nside8192_CAMB.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p5_nside8192_CAMB.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p6_nside8192_CAMB.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p7_nside8192_CAMB.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p8_nside8192_CAMB.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p7_0p9_nside8192_CAMB.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin10p7_1p0_nside8192_CAMB.pickle'],
                     labels=["0.0", "0.1", "0.2" ,"0.3","0.4","0.5","0.6","0.7","0.8","0.9","1.0"],
                     line_styles=["-" for _ in range(11)], 
                     colours=["tab:blue" for _ in range(11)],
@@ -259,7 +277,7 @@ if __name__ == '__main__':
                            file_methods=["unlensed", "lensed_z2", "lensed_z3"],
                            )
     quit()
-    tp.generic_plot(file_list=['./L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin11p6_nside8192_FITS_unlensed.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin11p6_nside8192_FITS_unlensed_no_ps.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin11p6_nside8192_FITS_lensed_z2.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin11p6_nside8192_FITS_lensed_z2_no_ps.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin11p6_nside8192_FITS_lensed_z3.pickle', './L1000N1800/Blue/HYDRO_FIDUCIAL_tau_Mstar_bin11p6_nside8192_FITS_lensed_z3_no_ps.pickle'],
+    tp.generic_plot(file_list=['./data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin11p6_nside8192_FITS_unlensed.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin11p6_nside8192_FITS_unlensed_no_ps.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin11p6_nside8192_FITS_lensed_z2.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin11p6_nside8192_FITS_lensed_z2_no_ps.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin11p6_nside8192_FITS_lensed_z3.pickle', './data_files/tau_profiles/L1000N1800//HYDRO_FIDUCIAL_tau_Mstar_bin11p6_nside8192_FITS_lensed_z3_no_ps.pickle'],
                     labels=["unlensed", "unlensed (no PS)", "lensed to z=2", "lensed to z=2 (no PS)", "lensed to z=3", "lensed to z=3 (no PS)"],
                     line_styles=["-", ":", "--", "*", "-.", "+"],
                     colours=["tab:blue" for _ in range(6)],
